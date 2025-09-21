a6128e37fc7c6d3409987d8466a451339292bacd
aycsi <207523432+aycsi@users.noreply.github.com>
2025-09-21 12:59:46 +0200

    commit-info

--- src/services/gitHistoryService.ts
+++ src/services/gitHistoryService.ts
@@ -1,6 +1,13 @@
 import * as vscode from 'vscode';
 import simpleGit, { SimpleGit } from 'simple-git';
 
+export interface CommitInfo {
+    hash: string;
+    message: string;
+    author: string;
+    date: Date;
+}
+
 export class GitHistoryService {
     private git: SimpleGit | null = null;
 
