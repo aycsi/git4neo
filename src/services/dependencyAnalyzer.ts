5b2c582d3a5e0d5f02f90834bb87563eff1f39c0
aycsi <207523432+aycsi@users.noreply.github.com>
2025-09-21 13:02:40 +0200

    new

---
+++ src/services/dependencyAnalyzer.ts
@@ -0,0 +1,33 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export interface DependencyInfo {
+    name: string;
+    version: string;
+    type: 'dependency' | 'devDependency' | 'peerDependency';
+}
+
+export class DependencyAnalyzer {
+    async analyzePackageJson(repoPath: string): Promise<DependencyInfo[]> {
+        const packageJsonPath = path.join(repoPath, 'package.json');
+        
+        if (!fs.existsSync(packageJsonPath)) {
+            return [];
+        }
+
+        const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+        const dependencies: DependencyInfo[] = [];
+
+        if (packageJson.dependencies) {
+            for (const [name, version] of Object.entries(packageJson.dependencies)) {
+                dependencies.push({
+                    name,
+                    version: version as string,
+                    type: 'dependency'
+                });
+            }
+        }
+
+        return dependencies;
+    }
+}
